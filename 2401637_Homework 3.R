de01f49c1e95f0f37b306fa066e841c9da928427
Azzah-27 <as24736@essex.ac.uk>
2025-02-22 20:20:43 +0000

    plotting graph

--- 2401637_Homework 3.R
+++ 2401637_Homework 3.R
@@ -10,3 +10,8 @@ rhyming_data_2 <- rhyming_data %>%
   unite(stimulus, image_1, image_2, sep = "_") %>% #this is to merge columns image_1 and image_2 into a new column named stimulus  
 mutate (participant_number = row_number()) %>%
  select(stimulus, rt, correct, type, VerbalScored, high_low_verbal, participant_number)
+
+#plotting data 
+ggplot(rhyming_data_2) + 
+  geom_bar(aes(x = factor(type) , fill = as_factor(high_low_verbal) ,  position= "dodge"))
+  
