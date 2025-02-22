dda1c40f2250d1a9aee12a31aa64a00c8acc53c0
Azzah-27 <as24736@essex.ac.uk>
2025-02-22 20:08:37 +0000

    tidying data

--- 2401637_Homework 3.R
+++ 2401637_Homework 3.R
@@ -5,5 +5,8 @@ library(tidyverse)
 #Load the data 
 rhyming_data <- read.csv('data/rhyming.csv')
 
-
-
+#tidying the data
+rhyming_data_2 <- rhyming_data %>%
+  unite(stimulus, image_1, image_2, sep = "_") %>% #this is to merge columns image_1 and image_2 into a new column named stimulus  
+mutate (participant_number = row_number()) %>%
+ select(stimulus, rt, correct, type, VerbalScored, high_low_verbal, participant_number)
