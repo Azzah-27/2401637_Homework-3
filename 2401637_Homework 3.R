4f0e0ce6bef9b55b16371dd1549e3c42e4295ebb
Azzah-27 <as24736@essex.ac.uk>
2025-02-23 14:48:52 +0000

    Testing with alpha= 5 and beta=6

--- 2401637_Homework 3.R
+++ 2401637_Homework 3.R
@@ -76,8 +76,8 @@ n <- 50
 x <- seq(0, 1, by = 0.01)
 
 # Define parameters for the Beta distribution
-alpha <- as.numeric(2)  
-beta <- as.numeric(2)   
+alpha <- as.numeric(5)  
+beta <- as.numeric(6)   
 
 # Calculate the probability density function (PDF) for the Beta distribution
 y <- dbeta(x, alpha, beta)
